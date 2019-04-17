978fe6ac61107995d935d4088d41a3a256d9b015
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-04-16 23:47:59 -0700

    updated toast messages

--- app/src/main/java/com/westfall/ad340_project/MainActivity.java
+++ app/src/main/java/com/westfall/ad340_project/MainActivity.java
@@ -2,6 +2,9 @@ package com.westfall.ad340_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.button14);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button #1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.button15);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button #2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btn3 = (Button)findViewById(R.id.button16);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button #3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btn4 = (Button)findViewById(R.id.button17);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button #4", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
