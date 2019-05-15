066f514bab6ffa25d965b561cfa7c208b2e8e204
Abdifatah.206 <shire2567@gmail.com>
2019-05-14 20:15:52 -0700

    added menu

--- app/src/main/java/com/westfall/ad340project/MainActivity.java
+++ app/src/main/java/com/westfall/ad340project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.westfall.ad340project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -45,4 +49,35 @@ public class MainActivity extends AppCompatActivity {
         }
     });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.about,menu);
+
+        // Intent E = new Intent(MainActivity.this, traffic_camera.class);
+
+        // startActivity(E);
+
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent E = new Intent(MainActivity.this, About.class);
+
+                startActivity(E);
+                //Toast.makeText(this,"item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
 }
