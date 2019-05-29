d52cb20cb40cc12e519b5054bb0d3415dd6cfb36
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-05-28 21:29:52 -0700

    updated project with working images for HW3

---
+++ app/src/main/java/com/westfall/ad340project/ZombieMovieDetails.java
@@ -0,0 +1,13 @@
+package com.westfall.ad340project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZombieMovieDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zombie_movie_details);
+    }
+}
