5044f9921a632088374738d5fbb0b9155f0d15cc
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-05-28 21:26:59 -0700

    updated project with working images

--- app/src/main/java/com/westfall/ad340project/MainActivity.java
+++ app/src/main/java/com/westfall/ad340project/MainActivity.java
@@ -3,25 +3,24 @@ package com.westfall.ad340project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    Button btn1 = (Button)findViewById(R.id.button1);
+    Button btn1 = (Button)findViewById(R.id.button);
     btn1.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Zombie Movies", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
+
         }
     });
 
@@ -29,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
     btn2.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Button #2", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Zombie Movies", Toast.LENGTH_SHORT).show();
+            openZombieMoviesPage();
+
         }
     });
 
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     btn3.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Button #3", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Button #2", Toast.LENGTH_SHORT).show();
         }
     });
 
@@ -45,39 +46,21 @@ public class MainActivity extends AppCompatActivity {
     btn4.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Button #3", Toast.LENGTH_SHORT).show();
+        }
+    });
+
+    Button btn5 = (Button)findViewById(R.id.button5);
+    btn5.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
             Toast.makeText(MainActivity.this, "Button #4", Toast.LENGTH_SHORT).show();
         }
     });
+}
+
+    public void openZombieMoviesPage() {
+        Intent intent = new Intent(this, ZombieMoviesPage.class);
+        startActivity(intent);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.about,menu);
-
-        // Intent E = new Intent(MainActivity.this, traffic_camera.class);
-
-        // startActivity(E);
-
-        return true;
-
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.item1:
-                Intent E = new Intent(MainActivity.this, About.class);
-
-                startActivity(E);
-                //Toast.makeText(this,"item 1 selected",Toast.LENGTH_SHORT).show();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-
-    }
-
 }
